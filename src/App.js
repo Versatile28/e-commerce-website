4ff7496d17a3a08a8474f9cfff312a9df4124074
Satyaki Pal <satyakipal111@gmail.com>
2025-02-19 17:46:53 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { useEffect } from "react";
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Landing from './components/Landing';
 import './App.css'
 
 export default function App() {
+
+   useEffect(() => {
+      document.title = "Homepage - Directory React Theme";
+  
+      const favicon = document.querySelector("link[rel='icon']");
+      if (favicon) {
+        favicon.href = "heart.png";
+      }
+    }, []);
+
   return (
     <div>
       <BrowserRouter basename='/e-commerce-website'>
