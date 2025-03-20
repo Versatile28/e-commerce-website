728feee964ce02b3ccb72e46708f067a2a523fcc
Satyaki Pal <satyakipal111@gmail.com>
2025-03-20 11:52:07 +0530

    minor fixes

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -7,7 +7,7 @@ export default function Cart({ showCart, setShowCart, handleCartClose }) {
   return (
     <div>
       <Offcanvas
-               className="overflow-auto d-none d-md-flex d-flex justify-content-start align-items-center w-40"
+               className="overflow-auto d-flex justify-content-start align-items-center cart-offcanvas"
                show={showCart}
                onHide={handleCartClose}
                placement="end"
@@ -15,9 +15,9 @@ export default function Cart({ showCart, setShowCart, handleCartClose }) {
             >
                <div className="w-100 px-5">
                   <Offcanvas.Header closeButton></Offcanvas.Header>
-                  {/* <Offcanvas.Title className="fw-bold pb-5">
+                  <Offcanvas.Title className="fw-bold pb-5">
                      Varkala
-                  </Offcanvas.Title> */}
+                  </Offcanvas.Title>
                   <Offcanvas.Title className="pb-3">Home</Offcanvas.Title>
                   <Offcanvas.Title className="pb-3">Link</Offcanvas.Title>
                   <Offcanvas.Title className="pb-3">Disabled</Offcanvas.Title>
