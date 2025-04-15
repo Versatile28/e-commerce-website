a769c6a65164deed82aedb3a737dcf9583a1da60
Satyaki Pal <satyakipal111@gmail.com>
2025-04-15 11:11:11 +0530

    minor fixes

--- src/components/HeroCarousel.jsx
+++ src/components/HeroCarousel.jsx
@@ -24,8 +24,8 @@ export default function HeroCarousel() {
          <SwiperSlide>
             <div className="slide-container first-slide carousel-image">
                <div className="slide-caption w-xs align-items-start p-7 padd-5">
-                  <h3 className="slide-highlight mt-5 mb-3">OUR ALL-TIME FAVOURITES</h3>
-                  <h1 className="slide-title mb-4">Blouses & Tops</h1>
+                  <p className="slide-highlight mt-5 mb-3">OUR ALL-TIME FAVOURITES</p>
+                  <p className="slide-title mb-4">Blouses & Tops</p>
                   <p className="slide-desc mb-5">
                      The bedding was hardly able to cover it and seemed ready to slide off any moment.
                   </p>
@@ -38,8 +38,8 @@ export default function HeroCarousel() {
          <SwiperSlide>
             <div className="slide-container second-slide carousel-image d-flex justify-content-center">
                <div className="d-flex justify-content-center flex-column align-items-center w-75">
-                  <h3 className="slide-highlight mb-3">BLUE & WHITE</h3>
-                  <h1 className="slide-title mb-4 text-center">Linen and Denim</h1>
+                  <p className="slide-highlight mb-3">BLUE & WHITE</p>
+                  <p className="slide-title mb-4 text-center">Linen and Denim</p>
                   <button className="slide-btn">START SHOPPING</button>
                </div>
             </div>
@@ -49,8 +49,8 @@ export default function HeroCarousel() {
             <div className="slide-container third-slide carousel-image">
                <div className='slide-caption d-none d-lg-flex'></div>
                <div className="slide-caption w-xs ps-lg-0 ps-5">
-                  <h3 className="slide-highlight mb-4 secondary-color-1">SNEAKERS</h3>
-                  <h1 className="slide-title mb-5">For every <br /> occasion</h1>
+                  <p className="slide-highlight mb-4 secondary-color-1">SNEAKERS</p>
+                  <p className="slide-title mb-5">For every <br /> occasion</p>
                   <button className="slide-btn">START SHOPPING</button>
                </div>
             </div>
